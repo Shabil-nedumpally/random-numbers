7f3a6aacc227844650c4df3956ae88c727b6cd1a
Shabil-nedumpally <shabilnptvr@gmail.com>
2022-04-01 17:04:35 +0530

    random

---
+++ number.js
@@ -0,0 +1,28 @@
+var number = Math.random(9999999999)*100000000
+var rand= Math.floor(number*10)
+var rand1= document.getElementById("randomNumbers")
+
+// var heading = document.createElement("h1");
+
+document.getElementById('heading').innerHTML = "+91 9"+ rand
+document.getElementById("heading2").innerHTML= "+91 8" + rand
+document.getElementById("heading3").innerHTML= "+91 7" + rand
+document.getElementById("heading4").innerHTML= "+91 6" + rand
+
+
+
+
+
+
+
+
+function oClick() {
+    
+
+    document.location.reload(true)
+}
+
+
+
+
+
